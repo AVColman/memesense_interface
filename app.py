d64a23c20a587103432e8ae1ee9325410aa8f141
alinacolman <alcolman.4@gmail.com>
2024-12-12 15:15:16 -0600

    changes

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import streamlit.components.v1 as components  # Import Streamlit
 import requests
 #import cv2
-import pytesseract
+#import pytesseract
 #from io import StringIO
 from PIL import Image
 import numpy as np
 
-API_URL = "http://127.0.0.1:8000/upload-image/"
+API_URL = "http://127.0.0.1:8000/predict/"
 
 st.set_page_config(
             page_title="MemeSense",
@@ -17,7 +17,7 @@ st.set_page_config(
 
 st.write("# Welcome to MemeSense! 👋")
 
-st.write("#### MemeSense is a project that classifies memes between the following categories: Positive, Negative and Neutral")
+st.write("#### MemeSense is a project that classifies memes between the following categories: Positive and Negative")
 
 st.write("")
 
@@ -34,66 +34,66 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     #Convert the image to a Numpy array
     img = np.array(image)
-
+    print(img.shape)
     # Convert the image to gray scale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Performing OTSU threshold
-    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+    #ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
     # Specify structure shape and kernel size.
     # Kernel size increases or decreases the area
     # of the rectangle to be detected.
     # A smaller value like (10, 10) will detect
     # each word instead of a sentence.
-    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+    #rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
     # Applying dilation on the threshold image
-    dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+    #dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
     # Finding contours
-    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
-                                                 cv2.CHAIN_APPROX_NONE)
+    #contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+   #                                              cv2.CHAIN_APPROX_NONE)
     # Creating a copy of image
-    im2 = img.copy()
+    #im2 = img.copy()
 
     #Variable to save the text of the meme
 
-    texto = ""
+    #texto = ""
 
 # Looping through the identified contours
 # Then rectangular part is cropped and passed on
 # to pytesseract for extracting text from it
 # Extracted text is then written into the text file
-    for cnt in contours:
-        x, y, w, h = cv2.boundingRect(cnt)
+   # for cnt in contours:
+   #     x, y, w, h = cv2.boundingRect(cnt)
 
     # Drawing a rectangle on copied image
-        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+   #     rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # Cropping the text block for giving input to OCR
-        cropped = im2[y:y + h, x:x + w]
+    #    cropped = im2[y:y + h, x:x + w]
 
     # Open the file in append mode
     #file = open("recognized.txt", "a")
 
 
     # Apply OCR on the cropped image
-        text = pytesseract.image_to_string(cropped)
+    #    text = pytesseract.image_to_string(cropped)
 
     # Appending the text into file
     #file.write(text)
     #file.write("\n")
-        texto = texto + " " + text
+    #    texto = texto + " " + text
 
 #Boton para enviar el archivo
 if st.button("Predict"):
-    if uploaded_file and texto:
+    if uploaded_file and #texto: #modificar esto
         files = {"image": uploaded_file.getvalue()}
-        data = {"text": texto}
-        response = requests.post("http://localhost:8000/upload_image", files=files, data=data)
+        #data = {"text": texto}
+        response = requests.post("http://localhost:8000/upload_image", files=files) #data=data)
         if response.status_code == 200:
             st.success("Request successful!")
         else:
             st.error("Request failed.")
     else:
-        st.write("You need to enter an image and a text!") #ver si se puede distinguir entre imagen y texto faltante
+        st.write("You need to enter an image!")
 
 #Altrnativa por si no corre el pytesseract:
 
